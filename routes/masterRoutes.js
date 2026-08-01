9e052dc4779e8e1d8f1505deb8b39c30c0b169d1
Harsh Kothari <kevalontechnology@gmail.com>
2026-08-02 00:54:15 +0530

    fix: allow Sales role to create factories, shipping lines, and ports

--- routes/masterRoutes.js
+++ routes/masterRoutes.js
@@ -11,13 +11,13 @@ router.get('/company', (req, res, next) => masterController.getCompany(req, res,
 router.put('/company', authorize(ROLES.ADMIN), (req, res, next) => masterController.updateCompany(req, res, next));
 
 router.get('/factories', (req, res, next) => masterController.getFactories(req, res, next));
-router.post('/factories', authorize(ROLES.ADMIN, ROLES.SUPERVISOR), (req, res, next) => masterController.createFactory(req, res, next));
+router.post('/factories', (req, res, next) => masterController.createFactory(req, res, next));
 
 router.get('/shipping-lines', (req, res, next) => masterController.getShippingLines(req, res, next));
-router.post('/shipping-lines', authorize(ROLES.ADMIN, ROLES.SUPERVISOR), (req, res, next) => masterController.createShippingLine(req, res, next));
+router.post('/shipping-lines', (req, res, next) => masterController.createShippingLine(req, res, next));
 
 router.get('/ports', (req, res, next) => masterController.getPorts(req, res, next));
-router.post('/ports', authorize(ROLES.ADMIN, ROLES.SUPERVISOR), (req, res, next) => masterController.createPort(req, res, next));
+router.post('/ports', (req, res, next) => masterController.createPort(req, res, next));
 
 router.get('/countries', (req, res, next) => masterController.getCountries(req, res, next));
 router.get('/currencies', (req, res, next) => masterController.getCurrencies(req, res, next));
